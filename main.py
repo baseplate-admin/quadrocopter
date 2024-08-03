d2238cd0fe0dc6f73987f99de01ee5be8ca19054
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-04 00:33:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from machine import Pin, I2C
 from vendor.bmp180 import BMP180
 from vendor.adxl345 import ADXL345
+from vendor.l3g4200 import L3G4200
 from src.motion_detector import MotionDetector
 
 
@@ -11,6 +12,8 @@ I2C_ID = const(1)
 while True:
     bmp180 = BMP180(sda=Pin(SDA_PIN), scl=Pin(SCL_PIN), i2c_id=I2C_ID)
     adxl345 = ADXL345(sda=Pin(SDA_PIN), scl=Pin(SCL_PIN), i2c_id=I2C_ID)
+    l3g4200d = L3G4200(sda=Pin(SDA_PIN),scl=Pin(SCL_PIN),i2c_id=I2C_ID)
+    print(l3g4200d.xyz_values())
     print(adxl345.xValue, adxl345.yValue, adxl345.zValue)
     print(MotionDetector(adxl345.xValue, adxl345.yValue, adxl345.zValue).direction)
     
