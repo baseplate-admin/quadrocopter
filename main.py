e3b6a73e2c4de9886952a62437c1757f797b7c4c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-06 18:57:13 +0600

    add

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-from machine import Pin, I2C
+from machine import Pin
 from vendor.bmp180 import BMP180
 from vendor.adxl345 import ADXL345
 from vendor.l3g4200 import L3G4200
-from src.motion_detector import MotionDetector
 
+import time
 
 SCL_PIN = const(21)
 SDA_PIN = const(20)
 I2C_ID = const(0)
 
+bmp180 = BMP180(sda=Pin(SDA_PIN), scl=Pin(SCL_PIN), i2c_id=I2C_ID)
+
+adxl345 = ADXL345(sda=Pin(SDA_PIN), scl=Pin(SCL_PIN), i2c_id=I2C_ID)
+adxl345.calibrate()
+
+l3g4200d = L3G4200(sda=Pin(SDA_PIN), scl=Pin(SCL_PIN), i2c_id=I2C_ID)
+
 while True:
-    bmp180 = BMP180(sda=Pin(SDA_PIN), scl=Pin(SCL_PIN), i2c_id=I2C_ID)
-    adxl345 = ADXL345(sda=Pin(SDA_PIN), scl=Pin(SCL_PIN), i2c_id=I2C_ID)
-    l3g4200d = L3G4200(sda=Pin(SDA_PIN),scl=Pin(SCL_PIN),i2c_id=I2C_ID)
-
-    print(adxl345.xValue, adxl345.yValue, adxl345.zValue)
-    print(adxl345.RP_calculate(adxl345.xValue, adxl345.yValue, adxl345.zValue))
-
-    
+    print(
+        (
+            adxl345.x,
+            adxl345.y,
+            adxl345.z,
+        )
+    )
+    print(adxl345.mean())
+    time.sleep(1)
     # print(bmp180.temperature)
--- vendor/adxl345.py
+++ vendor/adxl345.py
@@ -1,6 +1,5 @@
 from machine import Pin, I2C
 import math
-import time
 
 
 I2C_ADDR = const(0x53)
@@ -22,8 +21,10 @@ class ADXL345:
         b[0] = 8
         self.i2c.writeto_mem(self.addr, 0x2D, b)
 
+        self.calibrated_values = {"x": 0, "y": 0, "z": 0}
+
     @property
-    def xValue(self):
+    def x(self):
         buff = self.i2c.readfrom_mem(self.addr, regAddress, TO_READ)
         x = (int(buff[1]) << 8) | buff[0]
         if x > 32767:
@@ -31,7 +32,7 @@ class ADXL345:
         return x
 
     @property
-    def yValue(self):
+    def y(self):
         buff = self.i2c.readfrom_mem(self.addr, regAddress, TO_READ)
         y = (int(buff[3]) << 8) | buff[2]
         if y > 32767:
@@ -39,12 +40,51 @@ class ADXL345:
         return y
 
     @property
-    def zValue(self):
+    def z(self):
         buff = self.i2c.readfrom_mem(self.addr, regAddress, TO_READ)
         z = (int(buff[5]) << 8) | buff[4]
         if z > 32767:
             z -= 65536
         return z
+
+    def calibrate(self):
+        print("Calibrating accelerometer sensor")
+        __x = 0
+        __y = 0
+        __z = 0
+        for i in range(0, 10000):
+            __x += self.x
+            __y += self.y
+            __z += self.z
+            if i % 1000 == 0:
+                print(".", end="")
+
+        self.calibrated_values = {"x": __x / 10000, "y": __y / 10000, "z": __z / 10000}
+
+    def mean(self):
+        __x = self.x - self.calibrated_values["x"]
+        __y = self.y - self.calibrated_values["y"]
+        __z = self.z - self.calibrated_values["z"]
+
+        if __x > 360:
+            __x -= 360
+        elif __x < -360:
+            __x += 360
+
+        if __y > 360:
+            __y -= 360
+        elif __y < -360:
+            __y += 360
+
+        if __z > 360:
+            __z -= 360
+        elif __z < -360:
+            __z += 360
+        return {
+            "x": __x,
+            "y": __y,
+            "z": __z,
+        }
 
     def RP_calculate(self, x, y, z):
         roll = math.atan2(y, z) * 57.3
