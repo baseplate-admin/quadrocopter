62a1a2d0f614d54be274aa8d89ebc18541dbb2b2
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-07 21:33:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -33,52 +33,51 @@ roll = 0
 yaw = 0
 
 while True:
-    accel_x, accel_y, accel_z = adxl345.x, adxl345.y, adxl345.z
+    # accel_x, accel_y, accel_z = adxl345.x, adxl345.y, adxl345.z
     mag_x, mag_y, mag_z = mmc5883.x, mmc5883.y, mmc5883.z
-    gyro_x, gyro_y, gyro_z = (
-        l3g4200d.xyz_values["x"],
-        l3g4200d.xyz_values["y"],
-        l3g4200d.xyz_values["z"],
-    )
+    # gyro_x, gyro_y, gyro_z = (
+    #     l3g4200d.xyz_values["x"],
+    #     l3g4200d.xyz_values["y"],
+    #     l3g4200d.xyz_values["z"],
+    # )
 
-    omega_x = math.radians(gyro_x)
-    omega_y = math.radians(gyro_y)
-    omega_z = math.radians(gyro_z)
+    # omega_x = math.radians(gyro_x)
+    # omega_y = math.radians(gyro_y)
+    # omega_z = math.radians(gyro_z)
 
-    pitch_acc = math.atan2(accel_y, math.sqrt(accel_x**2 + accel_z**2))
-    roll_acc = math.atan2(-accel_x, accel_z)
+    # pitch_acc = math.atan2(accel_y, math.sqrt(accel_x**2 + accel_z**2))
+    # roll_acc = math.atan2(-accel_x, accel_z)
 
-    m_x_corr = mag_x * math.cos(pitch_acc) + mag_z * math.sin(pitch_acc)
-    m_y_corr = (
-        mag_x * math.sin(roll_acc) * math.sin(pitch_acc)
-        + mag_y * math.cos(roll_acc)
-        - mag_z * math.sin(roll_acc) * math.cos(pitch_acc)
-    )
+    # m_x_corr = mag_x * math.cos(pitch_acc) + mag_z * math.sin(pitch_acc)
+    # m_y_corr = (
+    #     mag_x * math.sin(roll_acc) * math.sin(pitch_acc)
+    #     + mag_y * math.cos(roll_acc)
+    #     - mag_z * math.sin(roll_acc) * math.cos(pitch_acc)
+    # )
 
-    yaw_mag = math.atan2(-m_y_corr, m_x_corr)
+    # yaw_mag = math.atan2(-m_y_corr, m_x_corr)
 
-    pitch_gyro = omega_y * delta_t
-    roll_gyro = omega_x * delta_t
-    yaw_gyro = omega_z * delta_t
+    # pitch_gyro = omega_y * delta_t
+    # roll_gyro = omega_x * delta_t
+    # yaw_gyro = omega_z * delta_t
 
+    # pitch = pitch_acc
+    # roll = roll_acc
+    # yaw = yaw_mag
 
-    pitch = pitch_acc
-    roll = roll_acc
-    yaw = yaw_mag
-    
-    # Combine gyroscope data for relative changes
-    pitch += pitch_gyro
-    roll += roll_gyro
-    yaw += yaw_gyro
+    # # Combine gyroscope data for relative changes
+    # pitch += pitch_gyro
+    # roll += roll_gyro
+    # yaw += yaw_gyro
 
-    # Convert to degrees for readability
-    pitch_deg = math.degrees(pitch)
-    roll_deg = math.degrees(roll)
-    yaw_deg = math.degrees(yaw)
+    # # Convert to degrees for readability
+    # pitch_deg = math.degrees(pitch)
+    # roll_deg = math.degrees(roll)
+    # yaw_deg = math.degrees(yaw)
 
     sleep_timer = _interval - int(time.ticks_ms() % _interval)
 
-    print(pitch_deg, roll_deg, yaw_deg)
-
+    # print(pitch_deg, roll_deg, yaw_deg)
+    print(mag_x, mag_y, mag_z)
     led.toggle()
     time.sleep_ms(sleep_timer)
